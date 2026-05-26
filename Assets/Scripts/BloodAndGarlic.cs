d87aa77e88bdb16a5f45135c6f6bb446bc54b1a7
Blue <comedemiv3rg4@gmail.com>
2026-05-26 20:45:34 +0200

    Agregado efecto hacia el enemigo

--- Assets/Scripts/BloodAndGarlic.cs
+++ Assets/Scripts/BloodAndGarlic.cs
@@ -15,7 +15,7 @@ public class BloodAndGarlic : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject player = GameObject.FindWithTag("Player");
+        GameObject player = GameObject.FindWithTag("Player"); //Saber desde el primer momento dónde está el player
         if (player != null)
         {
             playerTransform = player.transform; 
@@ -25,18 +25,30 @@ public class BloodAndGarlic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.Space) && inventory.Count > 0)
         {        
             Items itemActual = inventory[0];  // Primer ítem que haya en la lista de inventario
 
-            if (itemActual.distractedTime > 0) //Blood
+            #region //////BLOOD//////
+
+            if (itemActual.distractedTime > 0) 
             {
                 Debug.Log("Dejar en el suelo. Tiempo de distracción: " + itemActual.distractedTime);
 
-                Instantiate(bloodVial, playerTransform.position, Quaternion.identity); // Dejar frasco en la posición donde estaba el jugador
+                GameObject spawnedVial = Instantiate(bloodVial, playerTransform.position, Quaternion.identity); // Dejar frasco en la posición donde estaba el jugador
+
+                CircleCollider2D attractionArea = spawnedVial.AddComponent<CircleCollider2D>();  //Crear un collider para "cambiar el target" del NavMesh
+                attractionArea.isTrigger = true;
+                attractionArea.radius = 5f;
+                spawnedVial.name = "BloodAttractionPoint";
 
                 inventory.RemoveAt(0); 
             }
+            #endregion
+
+
+            #region  //////GARLIC//////
 
             else if (itemActual.wearingTime > 0)
             {
@@ -46,10 +58,15 @@ public class BloodAndGarlic : MonoBehaviour
                 invisibleGarlic.transform.SetParent(playerTransform); //"Pegar" ajo a player
                 invisibleGarlic.transform.localPosition = Vector3.zero;
 
+                CircleCollider2D repulsionArea = invisibleGarlic.AddComponent<CircleCollider2D>(); //Crear un collider (de nuevo) que espanta al vampiro
+                repulsionArea.isTrigger = true;
+                repulsionArea.radius = 4f;
+
                 Destroy(invisibleGarlic, itemActual.wearingTime);
 
                 inventory.RemoveAt(0);
             }
+            #endregion
         }
     }
 }
